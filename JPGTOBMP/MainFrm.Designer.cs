ce30d6908b3ee17ed43e266dfa866ccafb6cc7f8
jack13163 <18163132129@163.com>
2018-05-18 15:25:19 +0800

    提交

---
+++ JPGTOBMP/MainFrm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace JPGTOBMP
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
+            this.btnOpenDir = new System.Windows.Forms.Button();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.btnReadBmp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenDir
+            // 
+            this.btnOpenDir.Location = new System.Drawing.Point(26, 24);
+            this.btnOpenDir.Name = "btnOpenDir";
+            this.btnOpenDir.Size = new System.Drawing.Size(104, 32);
+            this.btnOpenDir.TabIndex = 0;
+            this.btnOpenDir.Text = "打开文件夹";
+            this.btnOpenDir.UseVisualStyleBackColor = true;
+            this.btnOpenDir.Click += new System.EventHandler(this.btnOpenDir_Click);
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(165, 24);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(104, 32);
+            this.btnTranslate.TabIndex = 1;
+            this.btnTranslate.Text = "转化";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // btnReadBmp
+            // 
+            this.btnReadBmp.Location = new System.Drawing.Point(12, 410);
+            this.btnReadBmp.Name = "btnReadBmp";
+            this.btnReadBmp.Size = new System.Drawing.Size(172, 32);
+            this.btnReadBmp.TabIndex = 2;
+            this.btnReadBmp.Text = "读取BMP像素数据";
+            this.btnReadBmp.UseVisualStyleBackColor = true;
+            this.btnReadBmp.Click += new System.EventHandler(this.btnReadBmp_Click);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 454);
+            this.Controls.Add(this.btnReadBmp);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.btnOpenDir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainFrm";
+            this.Text = "位图转换";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenDir;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.Button btnReadBmp;
+    }
+}
+
---
+++ JPGTOBMP/MainFrm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JPGTOBMP
+{
+    public partial class MainFrm : Form
+    {
+        private string path = "";
+        private static int i = 0;
+
+        public MainFrm()
+        {
+            InitializeComponent();
+            this.btnTranslate.Enabled = false;
+        }
+
+        private void btnOpenDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择JPG图片的最顶层文件夹";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                if (!string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    path = dialog.SelectedPath;
+                    this.btnTranslate.Enabled = true;
+                }
+            }
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += Bw_DoWork;
+
+            bw.RunWorkerAsync();
+        }
+
+        private void Bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //遍历文件夹下面的文件，将所有的JPG转换为BMP
+            getDirectory(path);
+            MessageBox.Show("转换成功");
+        }
+
+        /// <summary>
+        /// 以递归的方式遍历指定了路径下的所有目录名
+        /// </summary>
+        /// <param name="path">父目录</param>
+        public static void getDirectory(string path)
+        {
+            ChangeFormat(path);
+            DirectoryInfo root = new DirectoryInfo(path);
+            foreach (DirectoryInfo d in root.GetDirectories())
+            {
+                i = 0;
+                getDirectory(d.FullName);
+            }
+        }
+
+        /// <summary>
+        /// 将指定目录下的所有文件夹中的文件转换为BMP位图，并保存到当前目录下
+        /// </summary>
+        /// <param name="path"></param>
+        public static void ChangeFormat(string path)
+        {
+            DirectoryInfo root = new DirectoryInfo(path);
+            foreach (FileInfo f in root.GetFiles())
+            {
+                Bitmap bmp = new Bitmap(path + "\\" + f.Name);
+                Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height, PixelFormat.Format32bppArgb);
+                Graphics g = Graphics.FromImage(bmp2);
+                g.DrawImage(bmp, 0, 0);
+                bmp2.Save(path + "\\" + i + ".bmp", ImageFormat.Bmp);
+                i++;
+            }
+        }
+
+        private void btnReadBmp_Click(object sender, EventArgs e)
+        {
+            //Bitmap bmp = new Bitmap(@"H:\PICS\0.bmp");
+            //for(int i = bmp.Height - 1; i >= 0; i--)
+            //{
+            //    for(int j = 0; j < bmp.Width; j++)
+            //    {
+            //        Console.Write("{0} ", bmp.GetPixel(j, i).R);
+            //    }
+            //}
+        }
+    }
+}
